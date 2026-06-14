2bab88f9e7dd20f37d1f6cd2b9a77930852e1fac
stevenmarp <stevenmarp@sinerka.id>
2026-06-14 14:43:51 +0700

    Prepare Odoo 17 professional reports

---
+++ sm_professional_reports/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from . import sm_report_style
+from . import res_company
+from . import res_partner
+from . import res_config_settings
+from . import account_move
+from . import sale_order
+from . import purchase_order
+from . import stock_picking
+from . import ir_actions_report
